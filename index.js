db080da2f2ad0f53d74c360f3620ef80e479d10b
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-04 08:00:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution here!
+
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    cats;
+}
+
+function destructivelyPrependCat(name)
+{
+    cats.unshift("Bob");
+    cats;
+} 
+function destructivelyRemoveLastCat()
+
+{
+    cats.pop();
+    cats;
+}
+
+function destructivelyRemoveFirstCat()
+
+{
+    cats.shift();
+    cats;
+}
+ function appendCat(name){
+    var newArray = cats.slice();
+  
+  newArray.push(name)
+  return newArray
+  
+ }
+ function prependCat(name){
+    var newArray = cats.slice();
+  
+  newArray.unshift(name)
+  return newArray
+  
+ }
+
+ function removeLastCat(){
+    var newArray = cats.slice();
+  
+  newArray.pop();
+  return newArray
+  
+ }
+
+ function removeFirstCat(){
+    var newArray = cats.slice();
+  
+  newArray.shift();
+  return newArray
+  
+ }
